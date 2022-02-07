1d9f7f9cb7edac1aa78db86d66add204f4b85551
unknown <rewatiraman7250@gmail.com>
2022-02-07 17:14:00 +0530

    State Implement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
-function App() {
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/Expenses/ExpenseItem";
+const App = () => {
+  const expenses = [{ expenseDate : new Date(2021 ,2 ,29), expenseTitle : 'Car Insurance' , expenseAmount : 294.67} ,
+                    { expenseDate : new Date(2021 ,2 ,29), expenseTitle : 'Car Insurance' , expenseAmount : 294.67} ]
   return (
-    <div className="App">
-      <h1>Hi There</h1>
-      <ExpenseItem></ExpenseItem>
+    <div>
+      <h1>Lets Get started</h1>
+      <ExpenseItem title={expenses[0].expenseTitle} amount={expenses[0].expenseAmount} date={expenses[0].expenseDate}></ExpenseItem>
+      <ExpenseItem title={expenses[0].expenseTitle} amount={expenses[0].expenseAmount} date={expenses[0].expenseDate}></ExpenseItem>
     </div>
   );
 }
